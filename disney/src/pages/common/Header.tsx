84841a19784822e814e5986f3222888c6ff14fa4
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-03-07 18:01:22 +0900

    added shooting star

--- disney/src/pages/common/Header.tsx
+++ disney/src/pages/common/Header.tsx
@@ -1,3 +1,22 @@
+import { Center, Image } from "@chakra-ui/react";
+import { Night, Star } from "../../styles/common/star";
+
 export default function Header() {
-    return <h1>this is header</h1>;
+    return (
+        <Center w="100%" h="200px" alignItems="flex-end">
+            <Image
+                src="/resources/images/disney_logo.png"
+                w="150px"
+                h="80px"
+                objectFit="contain"
+            />
+            <Center position="absolute" top={0} left={"45%"}>
+                <Night className="night">
+                    {[...new Array(20)].map((_, i) => (
+                        <Star key={i} className="shooting_star" />
+                    ))}
+                </Night>
+            </Center>
+        </Center>
+    );
 }
