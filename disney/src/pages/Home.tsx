0b0e7f62c2d36c2394dfebb6313f5cda7fe595ea
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-03-07 20:53:33 +0900

    added get entire characters api

--- disney/src/pages/Home.tsx
+++ disney/src/pages/Home.tsx
@@ -1,12 +1,23 @@
+import { Text } from "@chakra-ui/react";
 import { Helmet } from "react-helmet";
+import { getCharacters } from "../api";
+import { useQuery } from "react-query";
 
 export default function Home() {
+    const characters = useQuery(["characters"], getCharacters);
+
+    if (characters.data) {
+        console.log(characters.data);
+    }
+
     return (
         <>
             <Helmet>
                 <title>Disney Characters</title>
             </Helmet>
-            <h1>HOME</h1>
+            <Text fontSize="50px" fontWeight="bold">
+                Disney Characters
+            </Text>
         </>
     );
 }
